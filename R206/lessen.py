6922b3324598f68e0ac987d28bcd3e75278f6ff7
mamaruru <0ashina0@gmail.com>
2018-01-26 17:21:56 +0900

    Add Source

---
+++ R206/lessen.py
@@ -0,0 +1,32 @@
+'''
+Created on 2018/01/26
+
+@author: 0ashi
+'''
+import string
+import random
+
+from Crypto.Cipher import AES
+
+def main():
+    print(AES.block_size)
+    print(string.ascii_letters)
+    
+    key = ''.join(
+        random.choice(string.ascii_letters) for _ in  range(AES.block_size))
+    
+    iv = ''.join(
+        random.choice(string.ascii_letters) for _ in  range(AES.block_size))
+    
+    plaintext = 'fgfdfhsgfshdfjhsdfkjjh'
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    padding_length = AES.block_size - len(plaintext) % AES.block_size
+    plaintext += chr(padding_length) * padding_length
+    cipher_text = cipher.encrypt(plaintext)
+    print(cipher_text)
+    
+    
+    
+if __name__ == '__main__':
+    main()
+    
